32362a2a303838feffc7b92795d476f68d7ce27f
Edu0liveira2 <eduardomo3@al.insper.edu.br>
2025-05-28 15:19:37 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main/main.c
+++ main/main.c
@@ -1,40 +1,77 @@
-/*
- * LED blink with FreeRTOS
- */
-#include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
-#include <queue.h>
-
-
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
 
-void task_1(void *p) {
-    while (1) {
-	printf("hello from task 1 \n");
-	vTaskDelay(pdMS_TO_TICKS(500));
+#include "tft_lcd_ili9341/ili9341/ili9341.h"
+#include "tft_lcd_ili9341/gfx/gfx.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#define P_BL   14
+#define P_ADC  26
+#define CH_ADC  1
+#define R_FIXO 10000u
+#define TAMQ     6
+
+static QueueHandle_t filaDados;
+
+void coleta(void *arg) {
+    uint16_t valor;
+    for (;;) {
+        adc_select_input(CH_ADC);
+        valor = adc_read();
+        xQueueSend(filaDados, &valor, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(400));
     }
 }
 
+void tela(void *arg) {
+    uint16_t entrada;
 
-void task_2(void *p) {
-    char cnt = 15;
-    while (1) {
+    for (;;) {
+        if (xQueueReceive(filaDados, &entrada, portMAX_DELAY)) {
+            uint32_t mv = (uint32_t)entrada * 3300 / 4095;
+            uint32_t res = mv > 0 ? (R_FIXO * 3300 / mv - R_FIXO) : 0;
+            uint32_t microA = (3300 - mv) * 1000 / R_FIXO;
 
-	printf("hello from task 2 \n");
-	vTaskDelay(pdMS_TO_TICKS(1000));
+            GFX_clearScreen();
+            GFX_setCursor(8, 12);
+            GFX_setTextColor(ILI9341_WHITE);
+            GFX_setTextBack(ILI9341_BLACK);
+            GFX_printf("U: %4u mV\n", mv);
+            GFX_setCursor(8, 38);
+            GFX_printf("R: %5u Ohm\n", res);
+            GFX_setCursor(8, 64);
+            GFX_printf("I: %4u uA\n", microA);
+            GFX_setCursor(8, 90);
+            GFX_printf("RAW: %u\n", entrada);
+            GFX_flush();
+        }
     }
 }
 
-int main() {
+int main(void) {
     stdio_init_all();
+    adc_init();
+    adc_gpio_init(P_ADC);
 
-    xTaskCreate(task_1, "Task 1", 4095, NULL, 1, NULL);
-    xTaskCreate(task_2, "Task 2", 4095, NULL, 1, NULL);
-    
+    gpio_init(P_BL);
+    gpio_set_dir(P_BL, GPIO_OUT);
+    gpio_put(P_BL, 1);
+
+    LCD_initDisplay();
+    LCD_setRotation(1);
+    GFX_createFramebuf();
+
+    filaDados = xQueueCreate(TAMQ, sizeof(uint16_t));
+    if (!filaDados) while (1);
+
+    xTaskCreate(coleta, "T1", 256, NULL, 2, NULL);
+    xTaskCreate(tela, "T2", 512, NULL, 1, NULL);
+
     vTaskStartScheduler();
-
-    while (true)
-        ;
+    while (true);
 }
